afde8c22e579ae9689cb6eaa7bf2eeae58ad52b0
kamos1 <keji.amos@gmail.com>
2017-05-08 07:38:47 -0600

    merge with dd

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import Card from './Card';
-import PropTypes from 'prop-types';
-import CompareCard from './CompareCard';
 
-const CardContainer = ({handleData, handleCompare, handleCompareData, handleComparison}) => {
-  const districtArray = Object.keys(handleData).map((districtObj) => <Card key={handleData[districtObj].location} {...handleData[districtObj]} handleCompare={handleCompare}/>);
-
-  const compareArray = handleCompareData.map((compareObj, index) => {
-    if (index === 2) {
-      return (
-      <CompareCard key={index} {...compareObj} />
-      )
-    }
-    return (
-    <Card key={compareObj.location} active={true} handleCompare={handleCompare} {...compareObj}/>
-    )
-
-  })
+const CardContainer = ({handleData}) => {
+  const keys = Object.keys(handleData.district);
 
   return(
-    <section className='cardsContainer'>
-      <div className='compareCards'>
-        {compareArray}
-      </div>
-      <div className='cardList'>
-        {districtArray}
-      </div>
+    <section>
+      {keys.map((key, index) => {
+        if (keys.length === 2) {
+          return (
+            <div key={handleData.district.location}>
+              <Card
+                location={handleData.district.location}
+                data={handleData.district.data}
+              />
+            </div>
+          );
+
+      } else {
+        return (
+          <div key={handleData.district[key].location}>
+            <Card
+              location={handleData.district[key].location}
+              data={handleData.district[key].data}
+            />
+          </div>
+        );
+       }
+      })}
     </section>
   )
-}
-
-CardContainer.propTypes = {
-  handleData: PropTypes.object,
-  handleCompare: PropTypes.func,
-  handleCompareData: PropTypes.array,
 }
 
 export default CardContainer;
